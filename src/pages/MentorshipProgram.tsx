4784e0a442bd1b4ae5282f36c1dbe4f1fb12cdae
georgegoldman <georgegoldmanjohn.o@gmail.com>
2025-06-08 08:49:07 +0100

    integrated the typeform

--- src/pages/MentorshipProgram.tsx
+++ src/pages/MentorshipProgram.tsx
@@ -1,11 +1,35 @@
+import { useEffect } from "react";
 import Header from "@/components/Header";
 
 const MentorshipProgram = () => {
-    return (
-        <div>
-            <Header />
-        </div>
-    )
-}
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://embed.typeform.com/next/embed.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div style={{ height: "100vh", margin: 0, padding: 0 }}>
+      {/* Optional: If you don't want the header, remove this line */}
+      <Header />
+
+      <div
+        data-tf-live="01JX77MSVYRPQBWYY443QAH2HX"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          width: "100%",
+        }}
+      ></div>
+    </div>
+  );
+};
 
 export default MentorshipProgram;
